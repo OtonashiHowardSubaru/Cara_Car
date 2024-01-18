530bae9fbcdc95aec7595b0eaf8f7adbb49a082a
yuhsinyu <yuhsin52411@gmail.com>
2024-01-18 19:44:09 +0800

    add footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,11 +1,46 @@
 <template>
   <footer class="mainFooter">
     <h1>我是footer的h1</h1>
-    <!-- {{ targetValue }} -->
-    {{ content1 }}
-    {{ content2 }}
-    <input type="number" v-model="money">
-    <Button @click="callMoney">給我錢</Button>
+    <img src="../assets/imgs/MainFooter/logo.png" alt="logo">
+    <div class="footerSection1">
+      <h4>與我們一起探索無限可能，<br>讓每個角落都成為冒險天地！</h4>
+      <div class="shopInfo">
+        <p>桃園市中壢區復興路46號8樓</p>
+        <p>03-425-1108</p>
+        <p>Cara_car@gmail.com</p>
+      </div>
+    </div>
+    <div class="footerSection2">
+        <ul>
+          <li>> 商品一覽</li>
+              <ul>
+                <li>> 兒童車</li>
+                <li>> 車車模型</li>
+                <li>> 其他配件</li>
+              </ul>
+          <li>> 二手車一覽</li>
+              <ul>
+                <li>> 我要販售</li>
+              </ul>    
+        </ul>
+        <ul>
+          <li>> 最新消息</li>
+          <li>> 關於我們</li>
+          <li>> 門市資訊</li>
+          <li>> 賽車小遊戲</li>
+        </ul>
+    </div>
+    <div class="footerSection3">
+      <div class="footerSection3Follow">
+        <span>Follow Us</span>
+        <img src="../assets/imgs/MainFooter/icon_fb.png" alt="">
+        <img src="../assets/imgs/MainFooter/icon_ig.png" alt="">
+      </div>
+      <div class="footerSection3Top">
+        <span>TOP</span>
+      </div>
+    </div>
+
 
   </footer>
 </template>
