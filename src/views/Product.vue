5b84d3259e37e9b82f2af9bdb35d3a70956efc2b
OtonashiHowardSubaru <gundamexia6610@gmail.com>
2024-01-18 20:41:28 +0800

    0118，api取法(有註解)

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -9,33 +9,22 @@ import axios from 'axios'; //引入涵式庫
         displayData: [],
       }
     },
-    computed:{
-      productCount(){
-        return Array.isArray(this.displayData)? this.displayData.length : 0
-      },
-      nodata(){
-        return this.responseData.length === 0
-      },
-      loading(){
-        return this.responseData.length === 0
-      },
-    },
     created() {
       this.axiosGetData();
     },
     methods: {
-      // fetchData(){
-      //   fetch('https://fakestoreapi.com/products')
-      //     .then(res=>res.json())
-      //     .then(json=>this.responseData = json)
-      // },
       axiosGetData(){
         axios.get('https://tibamef2e.com/cgd103/g1/api/getProducts.php?order=prod_name&limit=9&page=1')
           .then( res=> {
             console.log(res)
-            if(res && res.data){
-              this.responseData = res.data
-              this.displayData = res.data
+            if(res && res.data && res.data.prods){
+              this.responseData = res.data.prods
+              this.displayData = res.data.prods
+              //由於res是一整個方法，這個方法內有好幾個小東西，其中一個就是資料(data)
+              //所以要取值才是res.data，而學長姐的api預設有prods與prodCount
+              //所以才是displayData = res.data.prods
+            }else{
+              console.log('資料沒有回傳到displayData喔')
             }
           })
       },
@@ -44,21 +33,23 @@ import axios from 'axios'; //引入涵式庫
           // console.log(item);
           return item.title.includes(this.search)
         })
-        }
+      },
     },
   }
 </script>
 <template>
-  <div class="product_card" v-if="loading">
-
+  <div >
   </div>
-  <div v-for="item in displayData">   
-    <div class="pro_card_img">
-      <img :src="item.prod_img1" alt="">
-    </div>
-    <div class="pro_crad_info">
-      <h6>{{ item.prod_name }}</h6>
-      <p>{{ item.prod_price }}</p>
+  <!-- 用vfor迴圈出資料，而第一項資料是測試資料，所以用v-if="index > 0"直接從第二個資料開始取 -->
+  <div v-for="(item,index) in displayData" :key="item.prod_id">
+    <div v-if="index > 0" class="product_card">
+      <div class="pro_card_img">
+        <img :src="item.prod_img1" alt="">
+      </div>
+      <div class="pro_crad_info">
+        <h6>{{ item.prod_name }}</h6>
+        <p>{{ item.prod_price }}</p>
+      </div>
     </div>
   </div>
 
