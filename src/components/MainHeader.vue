b1ed31191613f0b1bd70b2d766fe8312ccebd145
a791474 <a791474@gmail.com>
2024-01-19 19:00:04 +0800

    登入燈箱寫在MainHeader.vue，login的scss尚未調完

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -10,12 +10,45 @@
         <div class="line"></div>
         <div class="indexHeaderLogin">
           <a href="#">
-            <img src="../assets/imgs/nav/nav-icon-Login.png" alt="" class="indexHeaderButtonLogin">
+            <img src="../assets/imgs/nav/nav-icon-Login.png" alt="" class="indexHeaderButtonLogin" @click="openModal">
           </a>
         </div>
       </ul>
     </nav>
   </header>
+  <!--  ↓登入燈箱↓  -->
+  <div id="loginOverlay" @click="closeModal"></div>
+  <div id="loginModal">
+    <span class="close" @click="closeModal">&times;</span>
+        <div class="login_container">
+            <fieldset>
+                <legend>會員登入</legend>
+                <p>帳號</p>
+                <input type="email" id="username" name="username" placeholder="電子郵件" required>
+                <p>密碼</p>
+                <input type="password" id="password" name="password" placeholder="************" required>
+                <a href="#">忘記密碼</a>
+                <div id="errorMessage" class="error-message"></div>
+                <button type="submit">登入</button>
+                <span>還不是會員嗎? <a href="#">立即註冊　！</a></span>
+            </fieldset>
+
+            <div class="other_login">
+                <h2>—————更多方式登入—————</h2>
+                <div class="login_group">
+                    <a href="#">
+                        <img src="../assets/imgs/login/loginGoogle.png" alt="otherLogin">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/imgs/login/loginFacebbok.png" alt="otherLogin">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/imgs/login/loginLine.png" alt="otherLogin">
+                    </a>
+                </div>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
@@ -23,8 +56,8 @@
 // import { RouterLink, RouterView } from 'vue-router'
 export default{
   // components: {
-  //   RouterLink,
-  //   RouterView,
+    // RouterLink,
+    // RouterView,
   // },
   data(){
     return{
@@ -35,7 +68,7 @@ export default{
         '../views/ShopInformationView.vue',
         '../views/MemberCenterView.vue',
         '../views/CartView.vue',
-    ],
+      ],
       img:[
         {i:'nav/nav-icon-01.png'},
         {i:'nav/nav-icon-02.png'},
@@ -43,18 +76,51 @@ export default{
         {i:'nav/nav-icon-04.png'},
         {i:'nav/nav-icon-05.png'},
         {i:'nav/nav-icon-06.png'},
-        // {i:'nav/nav-icon-Login.png'},
       ],
-      
     }
   },
-    methods:{
-      getImageUrl(paths){
-        return new URL(`../assets/imgs/${paths}`, import.meta.url).href
-      }
-    }
+  // created() {
+  //   this.axiosGetData();
+  // },
+  methods:{
+    getImageUrl(paths){
+      return new URL(`../assets/imgs/${paths}`, import.meta.url).href
+    },
+    // axiosGetData(){
+    //     axios.get('https://tibamef2e.com/cgd103/g1/api/getMember.php?type=front')
+    //         .then( res=> {
+    //         console.log(res)
+    //         if(res && res.data){
+    //             this.responseData = res.data
+    //             this.displayData = res.data
+    //         }
+    //     })
+    // },
+
+    openModal(){
+      const loginModal = document.querySelector('#loginModal')
+      const loginOverlay = document.querySelector('#loginOverlay')
+      loginOverlay.style.display = 'block';
+      loginModal.style.display = 'block';
+      loginOverlay.classList.add('fadeIn');
+      loginModal.classList.add('fadeIn');
+    },
+    closeModal(){
+        loginOverlay.style.display = 'none';
+        loginModal.classList.remove('fadeIn');
+        loginModal.classList.add('fadeOut');
+        loginOverlay.classList.remove('fadeIn');
+        loginOverlay.classList.add('fadeOut');
+
+        setTimeout(() => {
+        loginModal.classList.remove('fadeOut');
+        loginOverlay.classList.remove('fadeOut');
+        loginModal.style.display = 'none';
+        loginOverlay.style.display = 'none';
+        }, 100);
+    },
+  }
 }
-  
 </script>
 
 <style lang="scss" scoped>
