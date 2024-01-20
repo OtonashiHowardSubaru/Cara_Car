833443cb1ec7acd44942b414cb4411305c8b31a8
a791474 <a791474@gmail.com>
2024-01-20 13:49:53 +0800

    修改頁簽圖示、index.html中的title名稱、index.js有新增註解

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes: [...],
+//   scrollBehavior (to, from, savedPosition) {
+//     // return 期望滾動到哪個的位置
+// 		// 始终滚動到頂部
+//     return { top: 0 }
+//   }
+// })
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      // meta:{
+      //   title: 'Cara Car',
+      // }
     },
     {
       path: '/About',
       name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
@@ -72,7 +82,17 @@ const router = createRouter({
       name: 'NewsArticle',
       component: () => import('../views/NewsArticleView.vue')
     },
+    {
+      path: '/BackLogin',
+      name: 'BackLogin',
+      component: () => import('../views/BackLoginView.vue')
+    },
   ]
 })
-
+//以下為上方新增meta:{title:}去更改每個頁面的頁簽名稱才需要解開註解。
+// router.beforeEach(async (to, from) => {
+//   if( to.meta && to.meta.title){
+//       document.title = to.meta.title
+//   }
+// })
 export default router
