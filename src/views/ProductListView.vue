3612546601574001f7474d751f6744553df1262d
Lyn-Green <prentice9823@gmail.com>
2024-01-24 02:04:11 +0800

    fix: productList RWD

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -122,25 +122,26 @@ import axios from 'axios'; //引入函式庫
           <img :src="item.arrowImg" alt="" class="arrow">
         </div>
       </div>
-      
-      <div class="phone">
-        <img src="../assets/imgs/product/sh_process_contact.png" alt="">
-        <div class="tel">
-          <div class="contact_detail">
-            <span>TELEPHONE</span>
-            <span>連絡電話</span>
-            <div class="phoneNumber">
-              <img src="../assets/imgs/product/sh_process_tel.png" alt="">
-              <span>03 425 1108</span>
-            </div>
-          </div>
-
-          <div class="opening">
-            <span>每周一、二公休</span>
-          </div> 
-        </div>  
-      </div>
     </div>  
+
+    <div class="phone">
+      <img src="../assets/imgs/product/sh_process_contact.png" alt="">
+      <div class="tel">
+        <div class="contact_detail">
+          <span>TELEPHONE</span>
+          <span>連絡電話</span>
+          <div class="phoneNumber">
+            <img src="../assets/imgs/product/sh_process_tel.png" alt="">
+            <span>03 425 1108</span>
+          </div>
+        </div>
+
+        <div class="opening">
+          <span>每周一、二公休</span>
+        </div> 
+      </div>  
+    </div>
+
   </div>
 </template>
 
