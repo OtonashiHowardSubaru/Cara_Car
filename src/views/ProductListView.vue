eda61d1bf8f4633f11ea665eb4e850c19030bb33
OtonashiHowardSubaru <gundamexia6610@gmail.com>
2024-01-25 20:48:23 +0800

    商品卡元件化、價格篩選器元件化、

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,10 +1,16 @@
-<script>
-import axios from 'axios'; //引入函式庫
+  <script>
+  import axios from 'axios'; //引入函式庫
+  import ProductCard from '@/components/ProductCard.vue';
+  import PriceSorter from '@/components/PriceSorter.vue';
 
   export default {
+    components:{
+      ProductCard, PriceSorter,
+    },
     data(){
       return {
         search: '',
+        newSort: '',
         responseData : [],
         displayData: [],
         sh_contact: [
@@ -27,7 +33,6 @@ import axios from 'axios'; //引入函式庫
             arrowImg: "src/assets/imgs/product/sh_process_arrow.png"
           },
         ],
-
       }
     },
     created() {
@@ -49,104 +54,93 @@ import axios from 'axios'; //引入函式庫
             }
           })
       },
-      handleFilter(){
+      handleSortChange(newSort){
+        if (newSort === "0") {
+          this.displayData = this.responseData
+        } else if (newSort === "desc") {
+          this.displayData.sort((a, b) => b.prod_price - a.prod_price);
+        } else if (newSort === "asc"){
+          this.displayData.sort((a, b) => a.prod_price - b.prod_price);
+        };
+      },
+      handleFilter(){ //這是老師的標題搜尋範例，先留著也許哪天用的到
         this.displayData = this.responseData.filter((item)=>{
           // console.log(item);
           return item.title.includes(this.search)
         })
+      },
     },
-    handleFilter() {
-      this.displayData = this.responseData.filter((item) => {
-        // console.log(item);
-        return item.title.includes(this.search)
-      })
-    },
-    getProductImageUrl(imageFileName) {
-      // 返回完整的URL
-      return `https://tibamef2e.com/cgd103/g1/images/shop/${imageFileName}`;
-    }
-  },
-}
-</script>
-<template>
-  <div class="pro_list_title col-12">
-    <h1>新品專區</h1>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="store_name col-12">
-        <h2>CARA CAR</h2>
-        <select name="" id="priceSorter">
-          <option value="0"></option>
-          <option value="desc">價格　　↓</option>
-          <option value="asc">價格　　↑</option>
-          <div class="arrow_ttd"></div>
-        </select>
-      </div>
-      <div class="pro_list_filter col-12 col-md-1">
-        <button><div class="arrow_ltr"></div><p>促銷中</p></button>
-        <button><div class="arrow_ltr"></div><p>電動車</p></button>
-        <button><div class="arrow_ltr"></div><p>配件</p></button>
-        <button><div class="arrow_ltr"></div><p>模型車</p></button>
-      </div>
-      <div class="pro_card_list col-9 col-md-10">
-        <!-- 用vfor迴圈出資料，而第一項資料是測試資料，所以用v-for="item, in displayData.slice(1)"直接從第二個資料開始取 -->
-        <div class="product_card col-md-4" v-for="item, in displayData.slice(1)" :key="item.prod_id">
-          <div class="pro_card_img">
-            <img :src="getProductImageUrl(item.prod_img1)" alt="Product Image">
-          </div>
-          <div class="pro_crad_info">
-            <h6>{{ item.prod_name }}</h6>
-            <p>{{ item.prod_price }}</p>
-          </div>
+  }
+  </script>
+  <template>
+    <div class="pro_list_title col-12">
+      <h1>新品專區</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="store_name col-12">
+          <h2>CARA CAR</h2>
+          <PriceSorter 
+          @sortChange="handleSortChange"
+          />
+        </div>
+        <div class="pro_list_filter col-12 col-md-1">
+          <button><div class="arrow_ltr"></div><p>促銷中</p></button>
+          <button><div class="arrow_ltr"></div><p>電動車</p></button>
+          <button><div class="arrow_ltr"></div><p>配件</p></button>
+          <button><div class="arrow_ltr"></div><p>模型車</p></button>
+        </div>
+        <div class="pro_card_list col-9 col-md-10">
+          <ProductCard 
+          :displayData="displayData"
+          />
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="sh_contact">
-    <div class="sh_text">
-      <h4>舊車新生，回憶傳承</h4>
-      <p>如果您有二手車或是任何其他問題，歡迎聯繫我們。</p>
-    </div>
-      
-    <div class="cards">
-      <div class="contact_card" v-for="item in sh_contact">
-        <div class="card_info">
-          <img :src="item.img" alt="">
-          <div class="item_text">
-            <p>{{ item.shSubtitle }}</p>
-            <p>{{ item.context }}</p>
-          </div>
-            
-          <img :src="item.arrowImg" alt="" class="arrow">
-        </div>
+    <div class="sh_contact">
+      <div class="sh_text">
+        <h4>舊車新生，回憶傳承</h4>
+        <p>如果您有二手車或是任何其他問題，歡迎聯繫我們。</p>
       </div>
-    </div>  
-
-    <div class="phone">
-      <img src="../assets/imgs/product/sh_process_contact.png" alt="">
-      <div class="tel">
-        <div class="contact_detail">
-          <span>TELEPHONE</span>
-          <span>連絡電話</span>
-          <div class="phoneNumber">
-            <img src="../assets/imgs/product/sh_process_tel.png" alt="">
-            <span>03 425 1108</span>
+        
+      <div class="cards">
+        <div class="contact_card" v-for="item in sh_contact">
+          <div class="card_info">
+            <img :src="item.img" alt="">
+            <div class="item_text">
+              <p>{{ item.shSubtitle }}</p>
+              <p>{{ item.context }}</p>
+            </div>
+              
+            <img :src="item.arrowImg" alt="" class="arrow">
           </div>
         </div>
-
-        <div class="opening">
-          <span>每周一、二公休</span>
-        </div> 
       </div>  
+
+      <div class="phone">
+        <img src="../assets/imgs/product/sh_process_contact.png" alt="">
+        <div class="tel">
+          <div class="contact_detail">
+            <span>TELEPHONE</span>
+            <span>連絡電話</span>
+            <div class="phoneNumber">
+              <img src="../assets/imgs/product/sh_process_tel.png" alt="">
+              <span>03 425 1108</span>
+            </div>
+          </div>
+
+          <div class="opening">
+            <span>每周一、二公休</span>
+          </div> 
+        </div>  
+      </div>
+
     </div>
+  </template>
 
-  </div>
-</template>
+  <style lang="scss">
 
-<style lang="scss">
+  @import '@/assets/scss/page/productList.scss';
 
-@import '@/assets/scss/page/productList.scss';
-
-</style>
+  </style>
