83e9f3f262bb4e6ffe84c957513714825cacc7ac
yuhsinyu <yuhsin52411@gmail.com>
2024-01-25 23:48:02 +0800

    add aboutRWD . add pic into imgs/draw

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <h2>Test init</h2>
+    <h1><img src="../assets/imgs/about/aboutTitle.png" alt=""></h1>
+    <!-- <h2>Test init</h2> -->
     <div class="topPics">
       <img src="../assets/imgs/about/aboutTop1.png" alt="aboutIMG">
       <img src="../assets/imgs/about/aboutTop2.png" alt="aboutIMG">
       <img src="../assets/imgs/about/aboutTop3.png" alt="aboutIMG">
       <img src="../assets/imgs/about/aboutTop4.png" alt="aboutIMG">
     </div>
-    <div class="aboutUsC">
-      <img src="../assets/imgs/about/aboutUsC1.png" alt="">
-      <div class="aboutUsCon">
+    <div class="aboutUsContents">
+      <div class="aboutUsContentsPic">
+        <img src="../assets/imgs/about/aboutUsC1.png" alt="">
+      </div>
+      <div class="aboutUsContent">
         <img src="../assets/imgs/draw/person_love.png" alt="">
         <p>歡迎踏入 Cara Car，<br>一個充滿創意與歡樂的玩具車仙境。
         </p>
@@ -18,10 +20,12 @@
         </p>
       </div>
     </div>
-    <div class="aboutUsC">
-      <img src="../assets/imgs/about/aboutUsC2.png" alt="">
-      <div class="aboutUsCon">
-        <!-- <img src="../assets/imgs/draw/person_love.PNG" alt=""> -->
+    <div class="aboutUsContents">
+      <div class="aboutUsContentsPic">
+        <img src="../assets/imgs/about/aboutUsC2.png" alt="">
+      </div>
+      <div class="aboutUsContent">
+        <!-- <div class="car_caraPic"><img src="../assets/imgs/draw/car_caracar.png" alt=""></div> -->
         <p>我們致力於提供各種特色的玩具車，<br>讓每個孩子都能擁有專屬的玩具車，<br>展開一場屬於他們獨特的冒險旅程。
       </p>
       <p>我們相信，<br>每個孩子都應該擁有屬於自己的獨特遊戲體驗。
@@ -29,6 +33,7 @@
       </div>
 
     </div>
+    <div class="car_caraPic"><img src="../assets/imgs/draw/car_caracar.png" alt=""></div>
     <BtnEvent/>
     <BtnShopInfo/>
 
