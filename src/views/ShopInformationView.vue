7891b57ef689eda0f9e511f6306ff3ec16103f03
Lyn-Green <prentice9823@gmail.com>
2024-01-29 18:30:55 +0800

    feat: 引入 infoTextCards

--- src/views/ShopInformationView.vue
+++ src/views/ShopInformationView.vue
@@ -7,30 +7,11 @@
             </div>
         </div>
 
-        <section class="infoText" v-for="item in infoText">
+        <section class="infoText">
             <img src="/src/assets/imgs/shopInfo/infoTitle.svg" alt="infoTitle" class="infoTitle">
-            <div>
-                <p>
-                    <span>門市地址 :</span>
-                    <span>{{ item.shopAddress }}</span>
-                </p>
-                <p>
-                    <span>聯絡電話 :</span>
-                    <span>{{ item.contactNumber }}</span>
-                </p>
-                <p>
-                    <span>電子信箱 :</span>
-                    <span>{{ item.contactEmail }}</span>
-                </p>
-                <p>
-                    <span>營業時間 :</span>
-                    <span>{{ item.openTime }}</span>
-                </p>
-                <p>
-                    <span>公休日 :</span>
-                    <span>{{ item.holiday }}</span>
-                </p>
-            </div>
+
+            <InfoTextCards />
+            
             <ShopMap class="shopInfoMap" />
             <div class="shopInfoBtnGroup">
                 <BtnAboutUs />
@@ -45,6 +26,7 @@ import BtnAboutUs from '@/components/btn/BtnAboutUs.vue'
 import BtnEvent from '@/components/btn/BtnEvent.vue'
 import MainHeader from '@/components/Header.vue';
 import ShopMap from '@/components/ShopMap.vue';
+import InfoTextCards from '@/components/InfoTextCards.vue';
 
 
 export default {
@@ -53,6 +35,7 @@ export default {
         BtnEvent,
         MainHeader,
         ShopMap,
+        InfoTextCards,
     },
     data() {
         return{
