456d5f4c729b798f4a4066c724291254cb31a02f
remkfer <remkfer@gmail.com>
2024-01-29 10:08:09 +0800

    component :也許你喜歡、別人也逛過title，二手商品內頁RWD

---
+++ src/components/TitleMaybeYouLike.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data(){
+      return {
+        
+      }
+    }
+}
+
+</script>
+<template>
+    
+<div class="maybeYouLike">
+    <h4>也許你會喜歡</h4> 
+</div>
+
+</template>
+<style lang="scss" scope>
+@import '@/assets/scss/components/title.scss';
+</style>
+
+
+
+
+
+
+
+
+
+
