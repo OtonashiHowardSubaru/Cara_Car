e5442c3863d6b3fd7c78fd03057cbc7533ad953f
a791474 <a791474@gmail.com>
2024-01-31 05:02:36 +0800

    修正完二手商品卡圖片路徑方式

--- src/views/SecondHandListView.vue
+++ src/views/SecondHandListView.vue
@@ -5,6 +5,15 @@ import MainHeader from '@/components/Header.vue';
 import PageNumber from "@/components/btn/PageNumber.vue"
 import ProductCard from "@/components/ProductCard.vue"
 import PriceSorter from '@/components/PriceSorter.vue';
+import shProduct01 from '@/assets/imgs/product/sh_product_1.png'
+import shProduct02 from '@/assets/imgs/product/sh_product_2.png'
+import shProduct03 from '@/assets/imgs/product/sh_product_3.png'
+import shProduct04 from '@/assets/imgs/product/sh_product_4.png'
+import shProduct05 from '@/assets/imgs/product/sh_product_5.png'
+import shProduct06 from '@/assets/imgs/product/sh_product_6.png'
+import shProduct07 from '@/assets/imgs/product/sh_product_7.png'
+import shProduct08 from '@/assets/imgs/product/sh_product_8.png'
+import shProduct09 from '@/assets/imgs/product/sh_product_9.png'
   export default {
     components:{
       CardShProcess,MainHeader,PageNumber,ProductCard,PriceSorter
@@ -18,55 +27,55 @@ import PriceSorter from '@/components/PriceSorter.vue';
         productDisplayList: [],
         sh_product_list: [
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_1.png",
+              prod_img1:shProduct01,
               prod_name:"極光追逐者",
               prod_price:"3000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_2.png",
+              prod_img1:shProduct02,
               prod_name:"極速閃電車",
               prod_price:"5000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_3.png",
+              prod_img1:shProduct03,
               prod_name:"太空探險車",
               prod_price:"8000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_4.png",
-              prod_name:"科技漂移车",
+              prod_img1:shProduct04,
+              prod_name:"科技漂移車",
               prod_price:"6000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_5.png",
-              prod_name:"爆炸速度狂飆车",
+              prod_img1:shProduct05,
+              prod_name:"爆炸速度狂飆車",
               prod_price:"4000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_6.png",
+              prod_img1:shProduct06,
               prod_name:"魔法變形賽車",
               prod_price:"6000",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_7.png",
+              prod_img1:shProduct07,
               prod_name:"閃電漂移車",
               prod_price:"2500",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_8.png",
+              prod_img1:shProduct08,
               prod_name:"太空漫步車",
               prod_price:"3500",
               linkwhere:"/SecondHand"
             },
             {
-              prod_img1:"/src/assets/imgs/product/sh_product_9.png",
+              prod_img1:shProduct09,
               prod_name:"磁浮飛行車",
               prod_price:"5500",
               linkwhere:"/SecondHand"
@@ -103,6 +112,7 @@ import PriceSorter from '@/components/PriceSorter.vue';
     getProductImageUrl(imageFileName) {
       // 返回完整的URL
       return `https://tibamef2e.com/cgd103/g1/images/shop/${imageFileName}`;
+      // return new URL(`../assets/imgs/product${imageFileName}`);
     },
     handleSortChange(newSort){
         if (newSort === "0") {
