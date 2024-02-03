23dc86b3266ddc0ea318b7cd2eb0b258275acbdd
Lyn-Green <prentice9823@gmail.com>
2024-02-03 10:44:23 +0800

    feat: 新增小物件animation components

---
+++ src/components/animation/RedBird.vue
@@ -0,0 +1,12 @@
+<template>
+
+<div class="redBird">
+    <img src="/src/assets/imgs/draw/decoBird03.svg" alt="decoBird03.svg">
+
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/animation/redBird.scss';
+</style>
---
+++ src/components/animation/YellowBird.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="yellowBird">
+        <img src="/src/assets/imgs/draw/decoBird01.svg" alt="decoBird01.svg">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/animation/yellowBird.scss';
+</style>
