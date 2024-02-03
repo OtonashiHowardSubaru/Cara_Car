59e39ea153c171f487cdbeff0ea4de844638084d
yuhsinyu <yuhsin52411@gmail.com>
2024-02-03 16:06:45 +0800

    fix map . fix secondhandform

--- src/components/ShopMap.vue
+++ src/components/ShopMap.vue
@@ -20,7 +20,7 @@ export default {
         const map = L.map('shopMap').setView([24.957672860339283, 121.22503247550222], 16)
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             maxZoom: 19,
-	        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+	        attribution: ''
         }).addTo(map);
 
         var marker = L.marker([24.957672860339283, 121.22503247550222]).addTo(map);
