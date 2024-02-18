5b93f2f8a67ff482d73e70e566feccd0bdf93b62
Kelly0518 <kelly05182@gmail.com>
2024-02-18 16:12:57 +0800

    fix 購物車功能 & 購物車頁面切版

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -4,7 +4,12 @@ import MainHeader from '@/components/MainHeader.vue';
 // import TitleViewed from '@/components/TitleViewed.vue';
 import ProCardSwiper1 from '@/components/ProCardSwiper1.vue';
 import ProCardSwiper2 from '@/components/ProCardSwiper2.vue';
-import NumberSelect from '@/components/btn/BtnNumberSelect.vue';
+// import NumberSelect from '@/components/btn/BtnNumberSelect.vue';
+import DoubleCloud from "@/components/animation/DoubleCloud.vue";
+import BlueBird from "@/components/animation/BlueBird.vue";
+import GreenBird from "@/components/animation/GreenBird.vue";
+import YellowBird from "@/components/animation/YellowBird.vue";
+
 
 import product01 from '@/assets/imgs/product/product_1.png'
 import product02 from '@/assets/imgs/product/product_2.png'
@@ -18,10 +23,12 @@ import product09 from '@/assets/imgs/product/product_9.png'
 
 export default {
 components:{
-    MainHeader,NumberSelect,ProCardSwiper1,ProCardSwiper2
+    MainHeader,ProCardSwiper1,ProCardSwiper2,DoubleCloud,BlueBird,GreenBird,YellowBird,
+    // NumberSelect,
 },
 data(){
     return {
+        qtyValue:'',
         count: 1,
         expanded:false,
         cartItems: [],
@@ -110,10 +117,16 @@ created() {
    // 從LocalStorage中讀取購物車資料
     const cartData = JSON.parse(localStorage.getItem('cart'));
     if (cartData) {
-      this.cartItems = cartData; // 將資料存儲在Vue的data屬性中
+        this.cartItems = cartData; // 將資料存儲在Vue的data屬性中
     }
 },
 methods: {
+    handleQtyChange(index,increment) {
+        let qtyValue = parseInt(this.cartItems[index].quantity);
+        qtyValue = isNaN(qtyValue) || qtyValue < 1 ? 1 : qtyValue + increment;
+        // this.$refs['qtyInput_' + index][0].value = qtyValue;
+        this.updateQuantity(index, qtyValue)
+    },
     toggleCartContent(){
         this.expanded = !this.expanded;
     },
@@ -145,41 +158,53 @@ methods: {
     <main>
         <section class="cart">
             <div class="cartTitle">
-                <h1>購物車
-                </h1>
+                <h2>購物車
+                </h2>
+                <div class="cartBird">
+                <BlueBird class="cartBlueBird"/>
+                <GreenBird class="cartGreenBird"/>
+                <YellowBird class="cartYellowBird"/>
             </div>
+            </div>
+            <DoubleCloud class="cartCloud"/>
+            
+            <div class="cartProcess1">
+                <div class="cartProcessCircle">1</div>
+                <div class="cartLine"></div>
+                <div class="cartProcessCircle">2</div>
+                <div class="cartLine"></div>
+                <div class="cartProcessCircle">3</div>
+            </div>
+            <div class="cartProcess2">
+                <span class="cartProcessname">你的訂單</span>
+                <span class="cartProcessname">填寫資料</span>
+                <span class="cartProcessname">完成訂單</span>
+            </div>
+
             <div class="cartContent">
-                <span class="productName">商品名稱</span>
+                <span class="productName">商品名稱與單價</span>
                 <span class="count">數量</span>
                 <span class="countTotal">合計</span>
             </div>
-            <!-- 這裡是購物車內容 -->
+            <!-- 這裡是商品內容 -->
             <div class="productCard" v-for="(item, index) in cartItems" :key="index">
                 <img :src="(item.imageUrl)" alt="ProductImage">
                     <div class="proCardP">
                         <p class="pro_name">{{ item.name }}</p>
                         <p class="pro_price">${{ item.price }}</p>
                     </div>
-                    <!-- @change="updateQuantity(index, $event)" -->
-                    <NumberSelect
+                    <!-- <NumberSelect
                     :qtyValue="item.quantity" @change="updateQuantity(index, $event)"
-                    />
-                    <p class="proCount">{{ item.total}}</p>
+                    /> -->
+                    <div class="number_select">
+                        <input type="button" value="-" class="qtyMinus" @click="handleQtyChange(index,-1)">
+                        <input type="text" name="quantity" :value="item.quantity" class="qty" ref="`qtyInput_${index}`" @keydown.enter.prevent>
+                        <input type="button" value="+" class="qtyPlus" @click="handleQtyChange(index,1)">
+                    </div>
+                    <p class="proCount">{{ item.price*item.quantity}}</p>
             </div>
-            <!-- <div class="productCard">
-                    <img src="../assets/imgs/cart/cart_product_img.png" alt="ProductImage">
-                    <div class="proCardP">
-                        <p class="pro_name">閃電漂移車</p>
-                        <p class="pro_price">$2500</p>
-                    </div>
-                    <div class="countButton">
-                        <button @click="minus">-</button>
-                        <input type="number" v-model="count" @input="updataCount">
-                        <button @click="add">+</button>
-                    </div>
-                    <p class="proCount">$3,000</p>
-            </div> -->
             <!-- 結束 -->
+            <!-- 這是客製化車牌內容 -->
             <div class="cartContent">
                 <span class="custom">客製化</span>
             </div>
@@ -189,16 +214,19 @@ methods: {
                     <input type="text" maxlength="8" class="customNumber">
                 </div>
             </form>
+            <!-- 結束 -->
         </section>
         <section class="cartFunction">
             <div class="cartPrice">
                 <span class="cartFunctionTitle">小計</span>
+                <!-- 這裡要算小計 -->
                 <span class="cartFunctionTitle">$3,000</span>
             </div>
             <div class="cartPrice">
                 <span class="cartFunctionTitle">運費</span>
                 <span class="cartFunctionTitle">運費計算參考</span>
             </div>
+            <!-- 這裡要算加運費的總金額 -->
             <p class="cartCountTotal">合計金額：$3,500</p>
             <div class="cartMatter" @click="toggleCartContent">
                 <span class="cartFunctionTitle">運費計算及注意事項</span>
@@ -216,6 +244,10 @@ methods: {
                     <p>運送時間： 提供預期的運送時間範圍，並提供即時更新。</p>
                     <p>客戶支援： 提供客戶支援服務，處理問題和查詢。</p>
                 </div>
+            </div>
+            <div class="cartButton">
+                <button type="button" id="goToProduct">繼續購買</button>
+                <button type="button" id="goToCart">前往結帳</button>
             </div>
         </section>
         <form class="cartReceiptInformation">
@@ -236,12 +268,34 @@ methods: {
                     </select>
                     <input type="text" placeholder=" 中正區"  class="area">
                 </div>
-                <input type="text" placeholder="OO路O段O號O樓" class="cartInput"> 
+                <input type="text" placeholder="OO路O段O號O樓" class="cartInputRoad"> 
                 <p class="cartInputTitle">備註欄</p>
                 <textarea name="remark" id="remark" cols="20" rows="5"></textarea>
+                <span class="informationTitle">
+                    填寫付款資訊
+                </span>
+                <p class="cartInputTitle">信用卡卡號</p>
+                <div class="allCardNumber">
+                    <input type="text" name="cardNumber" class="cardNumber">
+                    <input type="text" name="cardNumber" class="cardNumber">
+                    <input type="text" name="cardNumber" class="cardNumber">
+                    <input type="text" name="cardNumber" class="cardNumber">
+                </div>
+                <p class="cartInputTitle">持卡人姓名</p>
+                <input type="text" name="name" class="cartInput">
+                <p class="cartInputTitle">帳單地址</p>
+                <div class="col66">
+                    <select name="city" id="city" >
+                        <option value="">請選擇縣市</option>
+                        <option v-for="item in city" :key="item">{{ (item).c }}</option>
+                    </select>
+                    <input type="text" placeholder=" 中正區"  class="area">
+                </div>
+                <input type="text" placeholder="OO路O段O號O樓" class="cartInputRoad">
                 <button type="submit" class="subButton">確認並送出訂單</button>
             </div>
         </form>
+        
     </main>
     <ProCardSwiper1 :displayData="productList" />
     <ProCardSwiper2 :displayData="productList" />
